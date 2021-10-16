0694d57152f51538aaf7d8eb322432c30fb5c50e
jadessu <junioradessu@gmail.com>
2021-10-16 16:07:50 -0500

    create commerse js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Products from './components/Products/Products';
+// import Products from './components/Products/Products';
+import { Products, Navbar} from "./components"
 function App() {
   return (
     <div className="App">
