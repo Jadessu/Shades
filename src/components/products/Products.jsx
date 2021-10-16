64224d47627ecfd201fb9b8b2432cb4317d54341
jadessu <junioradessu@gmail.com>
2021-10-16 15:54:10 -0500

    add css to products list

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import {Grid} from "@material-ui/core"
 import Product from "./Product/Product";
+import "./Product.css"
 const products = [
   {
     id: 1,
@@ -22,14 +23,15 @@ const products = [
 
 function Products() {
     return (
-        <main>
-            <Grid container justify="center" spacing={4}>
+        <main className="product-listing">
+            
                 {products.map((product) => (
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                    <div item key={product.id} >
+                       
                         <Product product={product}/>
-                        </Grid>
+                        </div>
                 ))}
-            </Grid>
+           
         </main>
     )
 }
