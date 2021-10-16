c9aa8375bb47f1c46dfe2c1742c67d28a650ae75
jadessu <junioradessu@gmail.com>
2021-10-16 16:16:44 -0500

    set up state to fetch products

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 // import Products from './components/Products/Products';
 import { Products, Navbar} from "./components"
+import { commerce } from "./lib/commerce"
+import { useState, useEffect} from "react"
+
 function App() {
+  const [products, setProducts] = useState([])
   return (
     <div className="App">
      <Products/>
