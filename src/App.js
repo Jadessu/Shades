188191611d50b3ce41b6b15494c51e900778e3f7
jadessu <junioradessu@gmail.com>
2021-10-17 14:33:36 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import './App.css';
 // import Products from './components/Products/Products';
-import { Products, Navbar} from "./components"
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import { Products} from "./components"
 import { commerce } from "./lib/commerce"
 import { useState, useEffect} from "react"
+import Navbar from "./components/navbar/Navbar"
 
 function App() {
   const [products, setProducts] = useState([])
+
+//   const fetchProducts = async ( )=> {
+//     const {data} = await commerce.products.list()
+//     setProducts(data)
+//   }
+
+//   useEffect( () => {
+// fetchProducts()
+//   }, [])
+  
   return (
     <div className="App">
-     <Products/>
+      <Router>
+        <Navbar />
+
+      <Switch>
+        {/* <Route exact path="/" component={Home}/> */}
+        
+
+      <Products/>
+        </Switch>
+      </Router>
+     
     </div>
   );
 }
---
+++ src/components/navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: "Skin",
+    path: "/skin",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Eyes",
+    path: "/eyes",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Lips",
+    path: "/lips",
+    cName: "dropdown-link",
+  },
+  {
+    title: "Tools",
+    path: "/tools",
+    cName: "dropdown-link",
+  },
+];
