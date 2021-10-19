27c5fdda2b87d2d3118ac156088e9b905b2bce28
jadessu <junioradessu@gmail.com>
2021-10-19 13:25:39 -0500

    create function to fetch cart

--- src/App.js
+++ src/App.js
@@ -7,15 +7,22 @@ import { useState, useEffect} from "react"
 import Navbar from './components/realNavbar/Navbar';
 function App() {
   const [products, setProducts] = useState([])
+  const [ cart, setCart] = useState({})
 
   const fetchProducts = async ()=> {
     const {data} = await commerce.products.list()
     setProducts(data)
-    console.log(data)
   }
 
+  const fetchCart = async () => {
+    const cart = await commerce.cart.retrieve()
+    setCart(cart)
+  }
+
+  
   useEffect( () => {
 fetchProducts()
+fetchCart()
   }, [])
   
   return (
