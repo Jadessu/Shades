c9fca7cffae309f6611e1610f6c1006e25fdf04d
jadessu <junioradessu@gmail.com>
2021-10-19 21:28:47 -0500

    add links to navbar cart

--- src/components/realNavbar/Navbar.jsx
+++ src/components/realNavbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography} from "@material-ui/core"
 import { ShoppingCart} from "@material-ui/icons"
 import useStyles from "./styles";
+import { Link } from "react-router-dom"
 
 
 
@@ -12,12 +13,12 @@ function Navbar({totalItems}) {
        <>
         <AppBar position = "fixed" className={classes.AppBar} color="inherit">
             <Toolbar>
-                <Typography variant="h6" className={classes.title} color="inherit">
+                <Typography component={Link} to ="/" variant="h6" className={classes.title} color="inherit">
                     Commerce.js
                 </Typography>
                 <div className={classes.grow}/>
                 <div className={classes.button}>
-                    <IconButton aria-label="Show cart items" color ="inherit">
+                    <IconButton component={Link} to="/cart"aria-label="Show cart items" color ="inherit">
                         <Badge badgeContent={totalItems} color="secondary">
                             <ShoppingCart/>
                         </Badge>
